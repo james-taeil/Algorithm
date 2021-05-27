5806ebda50ada6d1d0c30685632a4ad071c72a7f
ti <roseswallow13@gmail.com>
2021-05-27 18:47:06 +0900

    Test Commit

--- BOJ/다이나믹프로그램/피보나치2.py
+++ BOJ/다이나믹프로그램/피보나치2.py
@@ -3,18 +3,18 @@
 # F1, F2 = 0, 1
 
 # for i in range(N):
-#     F1, F2 = F2, F1 + F2
+#     F1, F2 = F3, F1 + F2
 
 # print(F1)
 
 
-N = int(input())
+# N = int(input())
 
-dp = [0 for i in range(N+1)]
+# dp = [0 for i in range(N+1)]
 
-dp[1] = 1
+# dp[1] = 1
 
-for i in range(2, N+1):
-    dp[i] = dp[i - 1] + dp[i - 2]
+# for i in range(2, N+1):
+#     dp[i] = dp[i - 1] + dp[i - 2]
 
-print(dp[N])
+# print(dp[N])
