52bcc5c214c6e4e56370adbfee33b28d010fa5c8
ti <roseswallow13@gmail.com>
2021-06-12 20:06:52 +0900

    다시 풀어보기

--- 나동빈 코테/그리디/거스름돈.py
+++ 나동빈 코테/그리디/거스름돈.py
@@ -3,14 +3,12 @@ coins = [500, 100, 50, 10]
 
 money = int(input())
 count = 0
-countCoin = 0
 
 
 for coin in coins:
     if money == 0:
         break
-    count = int(money / coin)
-    countCoin += count
-    money = money - (coin * count)    
+    count += money // coin
+    money %= coin    
     
-print(countCoin)
+print(count)
