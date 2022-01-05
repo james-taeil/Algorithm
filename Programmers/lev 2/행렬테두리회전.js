91d3823811b46b481c53f847f0c005ed654dbcf1
ti <roseswallow13@gmail.com>
2022-01-05 18:24:05 +0900

    [Algorithm] programmers lev2

--- Programmers/lev 2/행렬테두리회전.js
+++ Programmers/lev 2/행렬테두리회전.js
@@ -10,10 +10,29 @@ const solution = (rows, columns, queries) => {
         })
     })
 
-    console.log(arr);
+    queries.forEach((query) => {
+        const [x1, y1, x2, y2] = query.map(pos => pos - 1);
+        const dequeue = [];
+        
+        for(let i = y1; i < y2; i++) dequeue.push(arr[x1][i]);
+        for(let i = x1; i < x2; i++) dequeue.push(arr[i][y2]);
+        for(let i = y2; i > y1; i--) dequeue.push(arr[x2][i]);
+        for(let i = x2; i > x1; i--) dequeue.push(arr[i][y1]);
+        
+        answer.push(Math.min(...dequeue));
+        const temp = dequeue.pop();
+        dequeue.unshift(temp);
+
+        for(let i = y1; i < y2; i++) arr[x1][i] = dequeue.shift();
+        for(let i = x1; i < x2; i++) arr[i][y2] = dequeue.shift();
+        for(let i = y2; i > y1; i--) arr[x2][i] = dequeue.shift();
+        for(let i = x2; i > x1; i--) arr[i][y1] = dequeue.shift();
+    })
 
     return answer;
 }
+
+
 
 let rows = 6;
 let columns = 6;
