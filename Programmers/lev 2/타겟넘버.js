e522f7e9d67899728ff0bdf3264440e2017cdfb2
ti <roseswallow13@gmail.com>
2022-01-15 15:03:41 +0900

    [Algorithm] programmers lev2

---
+++ Programmers/lev 2/타겟넘버.js
@@ -0,0 +1,20 @@
+const solution = (numbers, target) => {
+    let answer = 0;
+    const dfs = (lev, sum) => {
+        if(lev === numbers.length) {
+            if(sum === target) answer += 1;
+            return;
+        }
+
+        dfs(lev + 1, sum + numbers[lev]);
+        dfs(lev + 1, sum - numbers[lev]);        
+    }
+    dfs(0, 0);
+
+    return answer;
+}
+
+
+let numbers = [1, 1, 1, 1, 1];
+let target = 3;
+console.log(solution(numbers, target));
