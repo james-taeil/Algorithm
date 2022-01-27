75e1b90250dfe309e3d898985fe81aff9fd58ced
ti <roseswallow13@gmail.com>
2022-01-27 18:14:41 +0900

    [Algorithm] programmers lev2

---
+++ Programmers/lev 2/모음사전.js
@@ -0,0 +1,17 @@
+function solution(word) {
+  const words = ["A", "E", "I", "O", "U"];
+  const dict = [];
+  
+  const getDict = (curWord, depth) => {
+      if (depth === 6) return;
+      dict.push(curWord);
+      for (let nextWord of words) {
+          getDict(curWord + nextWord, depth + 1);
+      }
+  }
+  
+  words.forEach((word) => {
+      getDict(word, 1);
+  })
+  return dict.indexOf(word) + 1;
+}
