1d763cd8f278a4c96286acfd44ec1b7b14519007
ti <roseswallow13@gmail.com>
2022-01-27 18:11:28 +0900

    [test]

--- Programmers/lev 2/멀쩡한사각형.js
+++ Programmers/lev 2/멀쩡한사각형.js
@@ -1,11 +1,10 @@
-const gcd = (w, h) => {
-    const mod = w % h;
-    
-    if(mod === 0) return h;
-    return gcd(h, mod);
-}
-
 const solution = (w, h) => {
+    const gcd = (w, h) => {
+        const mod = w % h;
+        
+        if(mod === 0) return h;
+        return gcd(h, mod);
+    }
     const gcdValue = gcd(w, h);
     return w * h - (w + h - gcdValue);
 }
