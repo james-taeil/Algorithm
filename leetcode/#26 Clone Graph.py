9c979ecc0f99bde4a0fa05a47453a1b1d79c4ecd
Tae-il Kim <71359732+james-taeil@users.noreply.github.com>
2022-06-21 20:26:11 +0900

    [Algorithm] Leetcode #26 Clone Graph

---
+++ leetcode/#26 Clone Graph.py
@@ -0,0 +1,17 @@
+def cloneGraph(self, node: 'Node') -> 'Node':
+        if not node:
+            return node
+        m, visited, stack = dict(), set(), deque([node])
+        while stack:
+            n = stack.pop()
+            if n in visited:
+                continue
+            visited.add(n)
+            if n not in m:
+                m[n] = Node(n.val)
+            for neigh in n.neighbors:
+                if neigh not in m:
+                    m[neigh] = Node(neigh.val)
+                m[n].neighbors.append(m[neigh])
+                stack.append(neigh)
+        return m[node]
